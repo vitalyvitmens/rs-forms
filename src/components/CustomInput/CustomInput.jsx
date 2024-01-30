79f4cfb57ffe630a4a26f2cda3688ce76622e378
vitalyvitmens <otm_@tut.by>
2024-01-31 00:04:38 +0300

    feat(src\components\CustomInput\CustomInput.jsx): добавляет CustomInput

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,12 +1,21 @@
 import { forwardRef } from 'react'
+import styles from './CustomInput.module.css'
 
 export const CustomInput = forwardRef((props, ref) => {
+	const asteriskStyle = {
+		color: 'red',
+		marginLeft: '2px',
+	}
+
 	return (
-		<div>
-			<label htmlFor={props.id}>{props.label}</label>
-			<span>{props.description}</span>
-			<br />
+		<div className={styles.customInput}>
+			<label className={styles.lable} htmlFor={props.id}>
+				{props.label}
+				{props.required && <span style={asteriskStyle}>*</span>}
+			</label>
+			<span className={styles.description}>{props.description}</span>
 			<input
+				className={styles.inputField}
 				{...props}
 				ref={ref}
 				style={{
@@ -14,6 +23,7 @@ export const CustomInput = forwardRef((props, ref) => {
 					fontSize: props.size + 'px',
 				}}
 			/>
+			<span className={styles.error}>{props.error}</span>
 		</div>
 	)
 })
